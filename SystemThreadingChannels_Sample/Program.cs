9745f76934859d67feb5375977bf7b33a4fc8afa
hamed hajiloo <hamedhajiloo@hotmail.com>
2021-04-01 16:19:37 +0430

    Add project files.

---
+++ SystemThreadingChannels_Sample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SystemThreadingChannels_Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
