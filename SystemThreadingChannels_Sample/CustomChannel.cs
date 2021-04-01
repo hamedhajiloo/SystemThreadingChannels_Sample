c9b54c10b6fd4d6ca8505070c27098bb70df9807
hamed hajiloo <hamedhajiloo@hotmail.com>
2021-04-01 16:32:29 +0430

    Created simple channel

---
+++ SystemThreadingChannels_Sample/CustomChannel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SystemThreadingChannels_Sample
+{
+    public class CustomChannel<T>
+    {
+        private readonly ConcurrentQueue<T> _queue = new ConcurrentQueue<T>();
+        private readonly SemaphoreSlim _sem = new SemaphoreSlim(0);
+
+        public void Write(T item)
+        {
+            _queue.Enqueue(item);
+            _sem.Release();
+        }
+
+        public async Task<T> ReadAsync()
+        {
+            await _sem.WaitAsync();
+            _queue.TryDequeue(out T item);
+            return item;
+        }
+    }
+}
--- SystemThreadingChannels_Sample/Program.cs
+++ SystemThreadingChannels_Sample/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace SystemThreadingChannels_Sample
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var customChannel = new CustomChannel<int>();
+            _ = Task.Run(async delegate
+            {
+                for (int i = 0; ; i++)
+                {
+                    await Task.Delay(1000);
+                    customChannel.Write(i);
+                }
+            });
+
+            while (true)
+            {
+                Console.WriteLine(await customChannel.ReadAsync());
+            }
         }
     }
 }
